d4fe6d2d1a6efae99eac063307597cd404a15dd8
Ivo Petkov <ivo@ivopetkov.com>
2018-12-30 06:11:20 +0200

    Fix in addon registration.

--- autoload.php
+++ autoload.php
@@ -9,6 +9,7 @@
 
 BearFramework\Addons::register('bearcms/share-button-element-addon', __DIR__, [
     'require' => [
-        'bearcms/bearframework-addon'
+        'bearcms/bearframework-addon',
+        'ivopetkov/social-sharing-bearframework-addon'
     ]
 ]);
